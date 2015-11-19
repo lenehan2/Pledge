37b29f74eb7dd391f96c19f17dbfb472dbca20ad
TheDooner64 <Robert.Muldoon.1@gmail.com>
2015-11-19 13:11:03 -0500

    Complete chapters 1-3

--- pledge.js
+++ pledge.js
@@ -3,9 +3,6 @@ Promises Workshop: build the pledge.js deferral-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
-
-
-
 var $Promise = function(){
 	this.state = "pending";
 	this.handlerGroups = [];
@@ -13,26 +10,23 @@ var $Promise = function(){
 
 var Deferral = function(){
 	this.$promise = new $Promise();
-	// this.$promise.state = "pending"
 
 };
 
 var defer = function() {
- 	return new Deferral();
+	return new Deferral();
 };
 
 Deferral.prototype.resolve = function(someData){
-	
 	this.$promise.value = this.$promise.state === "pending" ? someData : this.$promise.value;
 	this.$promise.state = this.$promise.state === "pending" ? "resolved" : this.$promise.state;
 
 	while(this.$promise.handlerGroups.length > 0) {
 		this.$promise.callHandlers(this.$promise.value);
 	}
-}
+};
 
 Deferral.prototype.reject = function(myReason) {
-	
 	this.$promise.value = this.$promise.state === "pending" ? myReason : this.$promise.value;
 	this.$promise.state = this.$promise.state === "pending" ? "rejected" : this.$promise.state;
 
@@ -41,23 +35,50 @@ Deferral.prototype.reject = function(myReason) {
 	}
 };
 
-$Promise.prototype.then = function(successCb,errorCb){
+$Promise.prototype.then = function(successCb, errorCb){
 	successCb = typeof successCb === "function" ? successCb : null;
 	errorCb = typeof errorCb === "function" ? errorCb : null;
-	this.handlerGroups.push({successCb: successCb, errorCb: errorCb});
+	newForwarder = new Deferral();
+	this.handlerGroups.push({successCb: successCb, errorCb: errorCb, forwarder: newForwarder});
 
 	if(this.state !== "pending"){
 		this.callHandlers(this.value);
+	} else {
+		return newForwarder.$promise;
 	}
+};
 
-}
+$Promise.prototype.catch = function(errorCb){
+	return this.then(null, errorCb);
+};
 
 $Promise.prototype.callHandlers = function(value) {
-	if(this.state === "resolved"){
-		this.handlerGroups[0].successCb(value);
-	}else{
-		this.handlerGroups[0].errorCb(value);
+	if(this.state === "resolved") {
+		if(typeof this.handlerGroups[0].successCb === "function") {
+			var resolvedVal;
+			try {
+				resolvedVal = this.handlerGroups[0].successCb(value);
+				if (this.handlerGroups[0].forwarder) this.handlerGroups[0].forwarder.resolve(resolvedVal);
+			} catch(e) {
+				if (this.handlerGroups[0].forwarder) this.handlerGroups[0].forwarder.reject(e);
+			}
+		} else {
+			if (this.handlerGroups[0].forwarder) this.handlerGroups[0].forwarder.resolve(value);
+		}
+	} else{
+		if (typeof this.handlerGroups[0].errorCb === "function") {
+			var rejectedVal;
+			try {
+				rejectedVal = this.handlerGroups[0].errorCb(value);
+				if (this.handlerGroups[0].forwarder) this.handlerGroups[0].forwarder.resolve(rejectedVal);
+			} catch(e) {
+				if (this.handlerGroups[0].forwarder) this.handlerGroups[0].forwarder.reject(e);
+			}
+		} else {
+			if (this.handlerGroups[0].forwarder) this.handlerGroups[0].forwarder.reject(value);
+		}
 	}
+
 	this.handlerGroups = this.handlerGroups.slice(1);
 };
 
